a86c267338cacc31d568cfd96b037eed70a15594
namsangwook <flowgrammer@gmail.com>
2016-04-19 18:50:37 +0900

    working on landing page - nav bar - landing page

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
   res.render("landing");
 });
+
+
 
 var port = process.env.PORT || 3003;
 app.listen(port, process.env.IP, function(){
